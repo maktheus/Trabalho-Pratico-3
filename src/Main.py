88ac9a1523b3415b976eef0ffc93bd4db88a2360
Matheus Serrao Uchoa-MSUCHOA1 <msuchoa1@motorola.com>
2023-10-15 12:55:18 -0400

    add read from txt fiile from args

--- src/Main.py
+++ src/Main.py
@@ -4,8 +4,7 @@ from FIFO import FIFO
 from LRU import LRU
 from SecondChance import SecondChance
 
-def main():
-    ref_string = "1,0;1,1;2,0;1,1;2,1;3,0;1,3;0,0"
+def main(ref_string):
     references = [(int(ref.split(',')[0]), int(ref.split(',')[1])) for ref in ref_string.split(';')[:-1]]
 
     algorithms = [FIFO(), LRU(), SecondChance()]
@@ -16,4 +15,16 @@ def main():
         print(f"{type(algo).__name__} Page Faults: ", algo.get_page_faults())
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        print("Usage: python3 <script_name>.py <input_file>.txt")
+        sys.exit(1)
+    
+    input_filename = sys.argv[1]
+    
+    try:
+        with open(input_filename, 'r') as file:
+            ref_string = file.read().strip()
+            main(ref_string)
+    except FileNotFoundError:
+        print(f"Error: File '{input_filename}' not found.")
+        sys.exit(1)
